c155df0d8adf6af92f97ad23cd9c6e2413bea9cc
Einzbern <ylwpaopao@163.com>
2018-08-09 10:17:04 +0800

    Revise winsorize to support side.

--- R/winsorize.R
+++ R/winsorize.R
@@ -3,39 +3,39 @@
 #' \code{winsorize()} winsorizes data to eliminate influence from outliers.
 #'
 #' @param x A numeric vector.
-#' @param method Default to "median". One of "median", "quantile", or "std".
+#' @param method Default to "quantile". One of "quantile", "median", or "std".
+#' @param side Side to winsorize. Default to "both". "both", "right", or "left".
 #'
 #' @return A numeric vector.
 #' @export
 #'
 #' @examples
 #' winsorize(c(1:10, 100))
-winsorize <- function(x, method = "median") {
-  if (method == "median") {
+winsorize <- function(x, method = "quantile", side = "both") {
+  if (method == "quantile") {
+    upper_bound <- quantile(x, 0.99, na.rm = TRUE)
+    lower_bound <- quantile(x, 0.01, na.rm = TRUE)
+  } else if (method == "median") {
     med <- median(x, na.rm = TRUE)
     diff_med <- median(abs(x - med), na.rm = TRUE)
-    # 当数据质量太差时，可能出现大部分因子值都是零的情形，取中位数都是零，这种情形下需要把零先忽略掉, 最后把以前是0的部分再恢复成0
-    if (diff_med == 0) {
-      index_is0 <- x == 0
-      x[index_is0] <- NA
-      med <- median(x, na.rm = TRUE)
-      diff_med <- median(abs(x - med), na.rm = TRUE)
-      x[x > med + 5 * diff_med] <- med + 5 * diff_med
-      x[x < med - 5 * diff_med] <- med - 5 * diff_med
-      x[index_is0] <- 0
-    } else {
-      x[x > med + 5 * diff_med] <- med + 5 * diff_med
-      x[x < med - 5 * diff_med] <- med - 5 * diff_med
-    }
+    upper_bound <- med + 5 * diff_med
+    lower_bound <- med - 5 * diff_med
   } else if (method == "std") {
     mu <- mean(x, na.rm = TRUE)
     sigma <- sd(x, na.rm = TRUE)
-    x[x > mu + 3 * sigma] <- mu + 3 * sigma
-    x[x < mu - 3 * sigma] <- mu - 3 * sigma
-  } else if (method == "quantile") {
-    q <- quantile(x, c(0.01, 0.99), na.rm = TRUE)
-    x[x > q[2]] <- q[2]
-    x[x < q[1]] <- q[1]
+    upper_bound <- mu + 3 * sigma
+    lower_bound <- mu - 3 * sigma
+  } else {
+    stop("Invalid method")
+  }
+
+  if (side == "right") {
+    x[x > upper_bound] <- upper_bound
+  } else if (side == "left") {
+    x[x < lower_bound] <- lower_bound
+  } else {
+    x[x > upper_bound] <- upper_bound
+    x[x < lower_bound] <- lower_bound
   }
 
   return(x)
